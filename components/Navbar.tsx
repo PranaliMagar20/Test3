6ec8312e06f798d4a3e0752235a4423bcc9a5c77
pranali magar <pranalimagar2023@gmail.com>
2025-02-28 20:26:13 +0530

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/IntelliMeet-logo.png"
+          width={128}
+          height={96}
+          alt="yoom logo"
+          className="max-sm:size-10"
+        />
+      </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
